f647a3a556313a6b7fc6fc6f108e4ca42cc87719
Lachlan Heywood <lachlan@realtheory.io>
2020-05-19 09:53:23 -0400

    add contact form

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const methodOverride = require('method-override');
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(methodOverride('_method'));
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
 app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'This is my Website',
+  });
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact', {
+    title: "Contact Me. Please. I'm lonely.",
+    submitted: false,
+  });
+});
+
+app.post('/contact', (req, res) => {
+  res.render('contact', {
+    title: 'Thank You',
+    submitted: true,
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Listening. Open http://localhost:${PORT} to view.`);
